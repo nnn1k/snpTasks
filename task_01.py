45160ba5f181e07eb5b41b34fe0971f4f460693f
nnn1k <nikitos.elidu@gmail.com>
2025-08-08 19:21:33 +0300

    initial commit

--- task_01.py
+++ task_01.py
@@ -1,2 +1,11 @@
-def is_palindrome(string):
-    return str(string) == str(string)[::-1]
+def is_palindrome(input_value):
+    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    if not isinstance(input_value, str):
+        input_value = str(input_value)
+
+    translator = str.maketrans('', '', punctuation + ' ')
+    cleaned = input_value.translate(translator).lower()
+
+    print(cleaned == cleaned[::-1])
+    return cleaned == cleaned[::-1]
+
