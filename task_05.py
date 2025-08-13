b64fbc82abaab07d7398473f0ac3e4bbda1a8164
nnn1k <nikitos.elidu@gmail.com>
2025-08-13 13:06:43 +0300

    initial commit

--- task_05.py
+++ task_05.py
@@ -2,5 +2,10 @@ from datetime import datetime, timedelta
 
 
 def date_in_future(days=0):
-    date = datetime.now() + timedelta(days=days)
-    return date.strftime("%d-%m-%Y %H:%M:%S")
+    if not isinstance(days, int):
+        date = datetime.now()
+    else:
+        date = datetime.now() + timedelta(days=days)
+    new_date = date.strftime("%d-%m-%Y %H:%M:%S")
+    return new_date
+
